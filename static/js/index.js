072eac30b34d95ad459f66a53e5301cf263ef5ed
Petrovich1973 <designtorg@ya.ru>
2021-10-29 01:13:36 +0300

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('.photo').zoom({url: 'static/media/pic_001.jpg', magnify: 2});
+    $('.threesixty').threeSixty({
+
+        // horizontal OR vertical
+        dragDirection: 'horizontal',
+
+        // use arrow keys
+        useKeys: true,
+
+        // is draggable?
+        draggable: true
+
+    });
 });
